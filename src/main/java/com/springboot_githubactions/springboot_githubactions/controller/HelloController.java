ae0c7049790a027637190a036a84f309b92047a2
Armando <armandojpr@yahoo.com>
2026-03-25 10:18:05 -0600

    Jugando

--- src/main/java/com/springboot_githubactions/springboot_githubactions/controller/HelloController.java
+++ src/main/java/com/springboot_githubactions/springboot_githubactions/controller/HelloController.java
@@ -5,14 +5,22 @@ import com.springboot_githubactions.springboot_githubactions.service.CalculatorS
 import lombok.AllArgsConstructor;
 import org.json.JSONArray;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RestController
 @RequestMapping("/api")
@@ -44,8 +52,23 @@ public class HelloController {
 
     @GetMapping("/people")
     public List<Person> persons() throws IOException {
-        ClassPathResource resource = new ClassPathResource("/jsons/people.json");
+        /*
+        ClassPathResource resource = new ClassPathResource("jsons/people.json");
         String str = Files.readString(resource.getFile().toPath());
+        */
+
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:jsons/people.json");
+
+
+        String str = null;
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            str = FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+
 
         JSONArray jsonArray = new JSONArray(str);
 
