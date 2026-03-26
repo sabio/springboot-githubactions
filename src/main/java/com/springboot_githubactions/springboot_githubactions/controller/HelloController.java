1c30bb0e924018abaf9ed2c61c172f637a65376d
Armando Jesus Gomez Parra <armandojpr@yahoo.com>
2026-03-25 19:46:46 -0600

    Jugando

--- src/main/java/com/springboot_githubactions/springboot_githubactions/controller/HelloController.java
+++ src/main/java/com/springboot_githubactions/springboot_githubactions/controller/HelloController.java
@@ -4,7 +4,6 @@ import com.springboot_githubactions.springboot_githubactions.dto.Person;
 import com.springboot_githubactions.springboot_githubactions.service.CalculatorService;
 import lombok.AllArgsConstructor;
 import org.json.JSONArray;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -12,9 +11,6 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -57,14 +53,8 @@ public class HelloController {
 
     @GetMapping("/people")
     public List<Person> persons() throws IOException {
-        /*
-        ClassPathResource resource = new ClassPathResource("jsons/people.json");
-        String str = Files.readString(resource.getFile().toPath());
-        */
-
         ResourceLoader resourceLoader = new DefaultResourceLoader();
         Resource resource = resourceLoader.getResource("classpath:jsons/people.json");
-
 
         String str = null;
         try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
